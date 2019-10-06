f619929c5fe08e6937f45b6bee7f8e361335730d
chinedum onyema <onyemachinedum@gmail.com>
2019-10-06 23:52:39 +0100

    <feat> set up connection to MongoDB Atlas and create a Thing data schema for every Thing put up for sale in the app

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+// MONGO CONNECTION = mongodb+srv://owi:<password>@cluster0-7cerh.mongodb.net/admin?retryWrites=true&w=majority
+// USER (owi) PW = KW1vE2szoTp0EeGY
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://owi:KW1vE2szoTp0EeGY@cluster0-7cerh.mongodb.net/admin?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.log(`${err}:  Failed to connect to MongoDB`);
+  });
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
