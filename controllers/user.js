6848e70fce033b00ebcd464132184f145b5eece1
chinedum onyema <onyemachinedum@gmail.com>
2019-10-13 00:31:50 +0100

    <feat> create a authentication route and its handler which allows users to signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,16 @@
-const user = require('../models/user');
+const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
 	bcrypt.hash(req.body.password, 10)
-		.then((hash) => {
-			const user = new user({
+		.then((hash) => { 
+			const user = new User({
 				email: req.body.email,
 				password: hash
 			});
+			console.log(user);
 			user.save()
-				.then(() => {
+				.then(() => {                                                                                                                                                                                                                    
 					res.status(201).json({
 						message: 'User added successfully!'
 					});
@@ -22,4 +23,35 @@ exports.signup = (req, res, next) => {
 		});
 };
 
-exports.login = (req, res, next) => { };
+exports.login = (req, res, next) => {
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			if (!user) {
+				return res.status(401).json({
+					error: new Error('User not found!')
+				});
+			}
+			bcrypt.compare(req.body.password, user.password)
+				.then((valid) => {
+					if (!valid) {
+						return res.status(401).json({
+							error: new Error('Incorrect password!')
+						});
+					}
+					res.status(200).json({
+						userId: user._id,
+						token: 'token'
+					});
+				})
+				.catch((error) => {
+					res.status(500).json({
+						error: error
+					});
+				});
+		})
+		.catch((error) => {
+			res.status(500).json({
+				error: error
+			});
+		});
+};
