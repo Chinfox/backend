6282ad82b89bd3244fa08b391ed54055325fbba9
chinedum onyema <onyemachinedum@gmail.com>
2019-10-13 02:00:41 +0100

    <feat> create an authentication middleware to protectand control access to selected routes

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
  
+const auth = require('../middleware/auth');
 const stuffController = require('../controllers/stuff');
 
-router.get('/', stuffController.getAllStuff);
-router.post('/', stuffController.createThing);
-router.get('/:id', stuffController.getOneThing);
-router.put('/:id', stuffController.modifyThing);
-router.delete('/:id', stuffController.deleteThing);
+router.get('/', auth, stuffController.getAllStuff);
+router.post('/', auth, stuffController.createThing);
+router.get('/:id', auth, stuffController.getOneThing);
+router.put('/:id', auth, stuffController.modifyThing);
+router.delete('/:id', auth, stuffController.deleteThing);
   
 module.exports = router;
