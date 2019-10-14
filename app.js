c67846a8dec0f3f91dbeca98b4ea027f4f70db80
chinedum onyema <onyemachinedum@gmail.com>
2019-10-14 03:28:39 +0100

    <feat> create a middleware to enable the POST and PUT routes for Things handle uploaded files properly

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 const stuffRoutes = require('./routes/stuff');
@@ -20,13 +21,16 @@ mongoose.connect('mongodb+srv://owiwi:b4CG56I70A68QaoP@cluster0-7cerh.mongodb.ne
     console.log(`${error}:  Failed to connect to MongoDB`);
   });
 
-app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
